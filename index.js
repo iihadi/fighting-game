28a9748e46f3542b76d40ad6169283dd83eccb4b
Abdul Hadi <31170019+iihadi@users.noreply.github.com>
2023-06-18 13:39:00 +0100

    added attacks, health bar, timer and gameover

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ canvas.width = 1024;
 canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
 
 class Sprite {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity, color = "red"}) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.width = 50;
+    this.lastKey;
+    this.attackBox = {
+      position: this.position,
+      width: 100,
+      height: 50,
+    }
+    this.color = color;
+    this.isAttacking;
   }
 
   draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, this.height);
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    //attack box
+    c.fillStyle = "blue";
+    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
   }
 
   update() {
@@ -30,6 +43,13 @@ class Sprite {
     } else {
       this.velocity.y += gravity;
     }
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
   }
 }
 
@@ -53,6 +73,7 @@ const enemy = new Sprite({
     x: 0,
     y: 0,
   },
+  color: 'green',
 });
 
 const keys = {
@@ -64,10 +85,17 @@ const keys = {
   },
   w: {
     pressed: false,
-  }
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
 };
-
-let lastKey;
 
 function animate() {
   c.fillStyle = "black";
@@ -76,11 +104,30 @@ function animate() {
   player.update();
   enemy.update();
 
+  //player 1 movement
   player.velocity.x = 0;
-  if (keys.a.pressed && lastKey === "a") {
-    player.velocity.x = -1;
-  } else if (keys.d.pressed && lastKey === "d") {
-    player.velocity.x = 1;
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
+  }
+
+  //player 2 movement
+  enemy.velocity.x = 0;
+  if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = 5;
+  }
+
+  //detecting collisions
+  if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x
+      && player.attackBox.position.x <= enemy.position.x + enemy.width
+      && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+      && player.attackBox.position.y <= enemy.position.y + enemy.height
+      &&  player.isAttacking
+    ){
+    console.log("collide");
   }
 }
 
@@ -88,22 +135,41 @@ animate();
 
 window.addEventListener("keydown", () => {
   switch (event.key) {
+
+    //enemy
     case "d":
       keys.d.pressed = true;
-      lastKey = "d";
+      player.lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
-      lastKey = "a";
+      player.lastKey = "a";
       break;
     case "w":
-      player.velocity.y = -7;
+      player.velocity.y = -20;
+      break;
+    case " ":
+      player.attack();
+      break;
+
+    //enemy  
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -20;
       break;
   }
 });
 
 window.addEventListener("keyup", () => {
   switch (event.key) {
+    //player 1 keys
     case "d":
       keys.d.pressed = false;
       break;
@@ -111,5 +177,13 @@ window.addEventListener("keyup", () => {
       keys.a.pressed = false;
     case "w":
       keys.w.pressed = false;
+    //player 2 keys
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+    case "ArrowUp":
+      keys.ArrowUp.pressed = false;
   }
 });
